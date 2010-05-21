f67e5e11bc0edb2fe1dbba3662d14a7ee061feaf
Seth Falcon <seth@userprimary.net>
2010-05-21 14:01:47 -0700

    Add custom.el to git tracking, remove duplicate user-specific

---
+++ custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(c-basic-offset 4)
+ '(c-default-style (quote ((c-mode . "bsd") (java-mode . "java") (awk-mode . "awk") (other . "gnu"))))
+ '(compilation-read-command nil)
+ '(ns-alternate-modifier (quote none))
+ '(ns-command-modifier (quote meta))
+ '(org-agenda-files (quote ("~/Notebook/org/bioc/advanced-course-may-2010.org" "~/Notebook/org/bioc/website.org" "~/Notebook/org/seth.org"))))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
