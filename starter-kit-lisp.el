94a68947b051345d66ad40113302878ceb754e6c
Seth Falcon <seth@userprimary.net>
2010-05-23 08:14:18 -0700

    Remove some paredit config

--- starter-kit-lisp.el
+++ starter-kit-lisp.el
@@ -42,17 +42,17 @@
 
 ;;; Enhance Lisp Modes
 
-(eval-after-load 'paredit
-  ;; need a binding that works in the terminal
-  '(define-key paredit-mode-map (kbd "M-)") 'paredit-forward-slurp-sexp))
+;; (eval-after-load 'paredit
+;;   ;; need a binding that works in the terminal
+;;   '(define-key paredit-mode-map (kbd "M-)") 'paredit-forward-slurp-sexp))
 
 (dolist (x '(scheme emacs-lisp lisp clojure))
   (when window-system
     (font-lock-add-keywords
      (intern (concat (symbol-name x) "-mode"))
      '(("(\\|)" . 'esk-paren-face))))
-  (add-hook
-   (intern (concat (symbol-name x) "-mode-hook")) 'turn-on-paredit)
+  ;; (add-hook
+  ;;  (intern (concat (symbol-name x) "-mode-hook")) 'turn-on-paredit)
   (add-hook
    (intern (concat (symbol-name x) "-mode-hook")) 'run-coding-hook))
 
