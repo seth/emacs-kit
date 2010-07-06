94e09ea93f9e6678dcb108a36cb05b48f17480a6
Seth Falcon <seth@userprimary.net>
2010-07-05 19:49:25 -0700

    No fancy "function" in js-mode

--- starter-kit-js.el
+++ starter-kit-js.el
@@ -17,12 +17,13 @@
   '(progn ;;(define-key espresso-mode-map "{" 'paredit-open-curly)
           ;;(define-key espresso-mode-map "}" 'paredit-close-curly-and-newline)
           ;; fixes problem with pretty function font-lock
-          (define-key espresso-mode-map (kbd ",") 'self-insert-command)
-          (font-lock-add-keywords
-           'espresso-mode `(("\\(function *\\)("
-                             (0 (progn (compose-region (match-beginning 1)
-                                                       (match-end 1) "ƒ")
-                                       nil)))))))
+     (define-key espresso-mode-map (kbd ",") 'self-insert-command)))
+;; don't replace 'function' with "ƒ", thanks.
+          ;; (font-lock-add-keywords
+          ;;  'espresso-mode `(("\\(function *\\)("
+          ;;                    (0 (progn (compose-region (match-beginning 1)
+          ;;                                              (match-end 1) "ƒ")
+          ;;                              nil)))))))
 
 (provide 'starter-kit-js)
 ;;; starter-kit-js.el ends here
