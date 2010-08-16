2680f4f88701ca65087682abdeda377c275a0652
Seth Falcon <seth@opscode.com>
2010-08-16 16:28:13 -0700

    Tweak ido mode to not guess filenames from point

--- starter-kit-misc.el
+++ starter-kit-misc.el
@@ -62,7 +62,7 @@
   (setq ido-enable-prefix nil
         ido-enable-flex-matching t
         ido-create-new-buffer 'always
-        ido-use-filename-at-point 'guess
+        ido-use-filename-at-point nil
         ido-max-prospects 10))
 
 (set-default 'indent-tabs-mode nil)
